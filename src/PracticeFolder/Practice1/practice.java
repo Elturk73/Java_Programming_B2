e87084688e8946f513c8b2777f0e16181ac98cf4
Elturk73 <TomTur0914!@>
2023-10-25 22:14:09 -0400

    My Commit on day 25.

---
+++ src/PracticeFolder/Practice1/practice.java
@@ -0,0 +1,17 @@
+package PracticeFolder.Practice1;
+
+public class practice {
+    public static void main(String[] args) {
+        int j = 7;
+        for (int i = 0; i < j-1; i +=2) {
+            System.out.println(i + " ");
+
+        }
+
+            }
+
+
+
+
+}
+
